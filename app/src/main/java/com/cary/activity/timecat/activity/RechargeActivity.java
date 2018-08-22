f34fa3190e4ae743e7ee80d7bdfe6948ba2514b4
方林 <841545680@qq.com>
2018-08-22 18:27:36 +0800

    add charge

---
+++ app/src/main/java/com/cary/activity/timecat/activity/RechargeActivity.java
@@ -0,0 +1,23 @@
+package com.cary.activity.timecat.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.cary.activity.timecat.BaseActivity;
+import com.cary.activity.timecat.R;
+
+public class RechargeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_recharge;
+    }
+}
---
+++ app/src/main/java/com/cary/activity/timecat/activity/WithDrawActivity.java
@@ -0,0 +1,20 @@
+package com.cary.activity.timecat.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.cary.activity.timecat.BaseActivity;
+import com.cary.activity.timecat.R;
+
+public class WithDrawActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_withdraw;
+    }
+}
